6afcdd91a4eca1ae38f66746676f1c7e72c02f35
mateush-souza <mateusguns798@gmail.com>
2024-05-24 20:27:22 -0300

    feat: responsive and style create

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,10 @@
-function clickMenu(){
-        items.style.display = "block";
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/src/assests/img/hamburguer.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "/src/assests/img/close.svg";
+    }
 }
